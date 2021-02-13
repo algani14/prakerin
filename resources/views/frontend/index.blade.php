ae63a7cd3233bf579e9da5cf8aa29ccf3abb335b
algani14 <alghani837@gmail.com>
2021-02-13 08:13:07 +0700

    first commit

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -39,15 +39,7 @@
 
   <!-- Main content -->
   <div class="main-content" id="panel">
-  <header class="header white-bg">
-      
-
-      <!--logo start-->
-      <a href="index.html" class="logo">Nice <span class="lite">Admin</span></a>
-      <!--logo end-->
-
-      
-    </header>
+  
     <div class="header bg-primary pb-6">
       <div class="container-fluid">
         <div class="header-body">
@@ -94,19 +86,13 @@
                 <div class="card-body">
                   <div class="row">
                     <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
+                    <div class="ml-auto"> <img src="../img2/happy-ipM.png" width="50" height="50" alt="Sembuh"> </div>
+                      <h5 class="card-title text-uppercase text-muted mb-0">Total Sembuh</h5>
                       <span class="h2 font-weight-bold mb-0">2,356</span>
                     </div>
                     <div class="col-auto">
-                      <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
-                        <i class="ni ni-chart-pie-35"></i>
-                      </div>
                     </div>
                   </div>
-                  <p class="mt-3 mb-0 text-sm">
-                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                    <span class="text-nowrap">Since last month</span>
-                  </p>
                 </div>
               </div>
             </div>
@@ -116,19 +102,13 @@
                 <div class="card-body">
                   <div class="row">
                     <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">Sales</h5>
+                    <div class="ml-auto"> <img src="../img2/sad-u6e.png" width="50" height="50" alt="Meninggal"> </div>
+                      <h5 class="card-title text-uppercase text-muted mb-0">Total Meninggal</h5>
                       <span class="h2 font-weight-bold mb-0">924</span>
                     </div>
                     <div class="col-auto">
-                      <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
-                        <i class="ni ni-money-coins"></i>
-                      </div>
                     </div>
                   </div>
-                  <p class="mt-3 mb-0 text-sm">
-                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                    <span class="text-nowrap">Since last month</span>
-                  </p>
                 </div>
               </div>
             </div>
@@ -138,19 +118,13 @@
                 <div class="card-body">
                   <div class="row">
                     <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">Performance</h5>
+                    <div class="ml-auto"> <img src="../img2/indonesia-PZq.png" width="50" height="50" alt="Indonesia"> </div>
+                      <h5 class="card-title text-uppercase text-muted mb-0">Indonesia</h5>
                       <span class="h2 font-weight-bold mb-0">49,65%</span>
                     </div>
                     <div class="col-auto">
-                      <div class="icon icon-shape bg-gradient-info text-white rounded-circle shadow">
-                        <i class="ni ni-chart-bar-32"></i>
-                      </div>
                     </div>
                   </div>
-                  <p class="mt-3 mb-0 text-sm">
-                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                    <span class="text-nowrap">Since last month</span>
-                  </p>
                 </div>
               </div>
             </div>
@@ -199,10 +173,9 @@
                   </tbody>
                 </table>
               </section>
-            </div>
-            <div>
-              
-    </div>
+             </div>
+            <div>        
+      </div>
     @Include ('layouts.components.footer')
   </div>
   
@@ -217,4 +190,10 @@
   <script src="assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
   <script src="assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
   <!-- Optional JS -->
-  <script src="assets/vendor/chart.js/dist/C
+  <script src="assets/vendor/chart.js/dist/Chart.min.js"></script>
+  <script src="assets/vendor/chart.js/dist/Chart.extension.js"></script>
+  <!-- Argon JS -->
+  <script src="assets/js/argon.js?v=1.2.0"></script>
+</body>
+
+</html>
